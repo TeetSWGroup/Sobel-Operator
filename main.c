ecd2254d4e160f8819e650b7a2cb36849f0837ed
tablejai <42264922+tablejai@users.noreply.github.com>
2019-11-11 15:59:44 +0800

    Update main.c

--- main.c
+++ main.c
@@ -10,40 +10,65 @@
 #include "camera.h"
 #include <math.h>
 
+uint16_t servo_location = 375;
 //sobel values
-const int sobel_operator_x[3][3] = {{47,0,-47},
-				    				{162,0,-162},
-									{47,0,-47}};
-const int sobel_operator_y[3][3] = {{47,162,47},
-				    				{0,0,0},
-									{-47,-162,-47}};
+//const int sobel_operator_x[3][3] = {{3,0,-3},
+//				    				{10,0,-10},
+//									{3,0,-3}};
+//const int sobel_operator_y[3][3] = {{3,10,3},
+//				    				{0,0,0},
+//									{-3,-10,-3}};
 //const int sobel_operator_x[3][3] = {{47,0,-47},
 //				    				{162,0,-162},
 //									{47,0,-47}};
 //const int sobel_operator_y[3][3] = {{47,162,47},
 //				    				{0,0,0},
 //									{-47,-162,-47}};
+//const int sobel_operator_x[3][3] = {{1,0,-1},
+//				    				{1,0,-1},
+//									{1,0,-1}};
+//const int sobel_operator_y[3][3] = {{1,1,1},
+//				    				{0,0,0},
+//									{-1,-1,-1}};
+//const int sobel_operator_x[3][3] = {{1,0,-1},
+//				    				{2,0,-2},
+//									{1,0,-1}};
+//const int sobel_operator_y[3][3] = {{1,2,1},
+//				    				{0,0,0},
+//									{-1,-2,-1}};
+//const int random_kernal[3][3] = {{1,0,1},
+//				    			{0,1,0},
+//								{1,0,1}};
+const int random_kernal[3][3] = {{1,0,1},
+				    			{0,1,0},
+								{1,0,1}};
+
 //to multiply the image array by the sobel values with respect to x and y
-uint8_t Sobel_operation_x(uint8_t top_left,uint8_t top,uint8_t top_right,uint8_t left,uint8_t mid,uint8_t right,uint8_t bottom_left,uint8_t bottom,uint8_t bottom_right){
-	uint8_t output = top_left * sobel_operator_x[0][0]+top * sobel_operator_x[0][1]+top_right * sobel_operator_x[0][2]+left * sobel_operator_x[1][0]+mid * sobel_operator_x[1][1]+right * sobel_operator_x[1][2]+bottom_left * sobel_operator_x[2][0]+bottom * sobel_operator_x[2][1]+bottom_right * sobel_operator_x[2][2];
-	return output;
-}
-uint8_t Sobel_operation_y(uint8_t top_left,uint8_t top,uint8_t top_right,uint8_t left,uint8_t mid,uint8_t right,uint8_t bottom_left,uint8_t bottom,uint8_t bottom_right){
-	uint8_t output = top_left * sobel_operator_y[0][0]+top * sobel_operator_y[0][1]+top_right * sobel_operator_y[0][2]+left * sobel_operator_y[1][0]+mid * sobel_operator_y[1][1]+right * sobel_operator_y[1][2]+bottom_left * sobel_operator_y[2][0]+bottom * sobel_operator_y[2][1]+bottom_right * sobel_operator_y[2][2];
+//uint8_t Sobel_operation_x(uint8_t top_left,uint8_t top,uint8_t top_right,uint8_t left,uint8_t mid,uint8_t right,uint8_t bottom_left,uint8_t bottom,uint8_t bottom_right){
+//	uint8_t output = top_left * sobel_operator_x[0][0]+top * sobel_operator_x[0][1]+top_right * sobel_operator_x[0][2]+left * sobel_operator_x[1][0]+mid * sobel_operator_x[1][1]+right * sobel_operator_x[1][2]+bottom_left * sobel_operator_x[2][0]+bottom * sobel_operator_x[2][1]+bottom_right * sobel_operator_x[2][2];
+//	return output;
+//}
+//uint8_t Sobel_operation_y(uint8_t top_left,uint8_t top,uint8_t top_right,uint8_t left,uint8_t mid,uint8_t right,uint8_t bottom_left,uint8_t bottom,uint8_t bottom_right){
+//	uint8_t output = top_left * sobel_operator_y[0][0]+top * sobel_operator_y[0][1]+top_right * sobel_operator_y[0][2]+left * sobel_operator_y[1][0]+mid * sobel_operator_y[1][1]+right * sobel_operator_y[1][2]+bottom_left * sobel_operator_y[2][0]+bottom * sobel_operator_y[2][1]+bottom_right * sobel_operator_y[2][2];
+//	return output;
+//}
+uint8_t kernal_operation(uint8_t top_left,uint8_t top,uint8_t top_right,uint8_t left,uint8_t mid,uint8_t right,uint8_t bottom_left,uint8_t bottom,uint8_t bottom_right){
+	uint8_t output = top_left * random_kernal[0][0]+top * random_kernal[0][1]+top_right * random_kernal[0][2]+left * random_kernal[1][0]+mid * random_kernal[1][1]+right * random_kernal[1][2]+bottom_left * random_kernal[2][0]+bottom * random_kernal[2][1]+bottom_right * random_kernal[2][2];
 	return output;
 }
 //centre_position is the centre of the image array
 uint8_t Left_Centre_distance(uint8_t centre_position,uint8_t image[][CAM_FrameWidth()-4]){
-	for(uint8_t distance = centre_position; distance> 0;distance--){
-//255 is white in grey scale
-		if(image[0][distance]==255){
+	for(uint8_t distance = centre_position; distance>= 0;distance--){
+		if(image[59][distance]==0){
+			distance = centre_position - distance;
 			return distance;
 		}
 	}
 }
 uint8_t Right_Centre_distance(uint8_t centre_position,uint8_t image[][CAM_FrameWidth()-4]){
 	for(uint8_t distance = centre_position; distance< CAM_FrameHeight();distance++){
-		if(image[0][distance]==255){
+		if(image[59][distance]==0){
+			distance = distance = centre_position;
 			return distance;
 		}
 	}
@@ -54,7 +79,7 @@ uint16_t difference_of_magenetic_sensor_values(){
 	return difference;
 }
 void SystemClock_Config(void);
-uint8_t main(void)
+void main(void)
 {
 //inits that I don't know what are the uses for
   HAL_Init();
@@ -79,14 +104,16 @@ uint8_t main(void)
   uint16_t left_magnetic_sensor;
   uint16_t right_magnetic_sensor;
   uint8_t difference_of_magnetic_values;
-  uint16_t servo_location;
 //defining variables for sobel_operator and median filter
   uint8_t grey_image[Height][Width];
   uint16_t rgb_image[Height][Width];
   uint8_t grey_image_sobel_x[Height-2][Width-2];
   uint8_t grey_image_sobel_y[Height-2][Width-2];
   uint8_t grey_image_sobel[Height-2][Width-2];
-  uint8_t final_image[CAM_FrameHeight()-4][CAM_FrameWidth()-4];
+  uint8_t final_image[Height-4][Width-4];
+  uint8_t grey_kernal_image[Height-2][Width-2];
+  CAM_SetBrightness(20);
+  CAM_SetContrast(100);
   uint8_t find_median_in3x3(uint8_t matrix[3][3])
   {
 	  uint8_t temp[9];
@@ -97,16 +124,20 @@ uint8_t main(void)
 		  temp[i+3]=matrix[1][i];
 		  temp[i+6]=matrix[2][i];
 	  }
-	  for(uint8_t j=0; j<8; j++)
+	  for (uint8_t k=0;k<8;k++)
 	  {
-		  if(temp[j]>temp[j+1]){temp_num=temp[j];temp[j]=temp[j+1];temp[j+1]=temp_num;}
+		 for(uint8_t j=0; j<8; j++)
+		  {
+			if(temp[j]>temp[j+1]){temp_num=temp[j];temp[j]=temp[j+1];temp[j+1]=temp_num;}
+		  }
 	  }
+
 	  return temp[4];
   }
   uint8_t median_filter_remastered(uint8_t image[][Width-2]){
 	  uint8_t temp[3][3];
-	  for(uint8_t y = 1;y<(CAM_FrameHeight()-2);y++){
-		  for(uint8_t x = 1;x < (CAM_FrameWidth() -2);x++){
+	  for(uint8_t y = 1;y<(Height-3);y++){
+		  for(uint8_t x = 1;x < (Width-3);x++){
 			  temp[0][0]=image[y-1][x-1];
 			  temp[0][1]=image[y-1][x];
 			  temp[0][2]=image[y-1][x+1];
@@ -120,10 +151,8 @@ uint8_t main(void)
 		  }
 	  }
   }
-
   while(1) {
 //set the wheels facing to the centre
-	  servo_location = 375;
 	  HAL_TIM_PWM_SetCompare(SERVO_TIM, TIM_CHANNEL_1, servo_location);
 //To make sure the Camera is on when capturing the grey image
 	  left_magnetic_sensor = ADC_Values[0][2];
@@ -133,19 +162,20 @@ uint8_t main(void)
 	  }
 	  for(uint8_t y= 1;(y < CAM_FrameHeight() - 1);y++){
 		  for(uint8_t x = 1; (x < CAM_FrameWidth() - 1); x++){
-			  grey_image_sobel_x[y][x] = Sobel_operation_x(grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1],grey_image[y][x-1],grey_image[y][x],grey_image[y][x+1],grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1]);
-			  grey_image_sobel_y[y][x] = Sobel_operation_y(grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1],grey_image[y][x-1],grey_image[y][x],grey_image[y][x+1],grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1]);
-			  grey_image_sobel[y][x] = sqrt(grey_image_sobel_x[y][x]*grey_image_sobel_x[y][x]+grey_image_sobel_y[y][x]*grey_image_sobel_y[y][x]);
-			  if(grey_image_sobel[y][x]>123){
-				  grey_image_sobel[y][x] = 255;
+//			  grey_image_sobel_x[y][x] = Sobel_operation_x(grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1],grey_image[y][x-1],grey_image[y][x],grey_image[y][x+1],grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1]);
+//			  grey_image_sobel_y[y][x] = Sobel_operation_y(grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1],grey_image[y][x-1],grey_image[y][x],grey_image[y][x+1],grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1]);
+//			  grey_image_sobel[y][x] = sqrt(grey_image_sobel_x[y][x]*grey_image_sobel_x[y][x]+grey_image_sobel_y[y][x]*grey_image_sobel_y[y][x]);
+			  grey_kernal_image[y][x] = kernal_operation(grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1],grey_image[y][x-1],grey_image[y][x],grey_image[y][x+1],grey_image[y-1][x-1],grey_image[y-1][x],grey_image[y-1][x+1]);
+			  if(grey_kernal_image[y][x]>120){
+				  grey_kernal_image[y][x] = 255;
 			  }
 			  else{
-				  grey_image_sobel[y][x] = 0;
+				  grey_kernal_image[y][x] = 0;
 			  }
 		  }
 	  }
-	  CAM_GreyToRGB565(grey_image_sobel, rgb_image);
-	  tft_print_image(rgb_image, 0, 0, CAM_FrameWidth()-2, CAM_FrameHeight()-2);
+	  CAM_GreyToRGB565(grey_kernal_image, rgb_image);
+	  tft_print_image(rgb_image, 0, 0, CAM_FrameWidth()-2, CAM_FrameHeight()-2 );
 	  if(tft_update(50)==0){
 		  tft_prints(0, 5, "%d",ADC_Values[0][0]);
 		  tft_prints(0, 6, "%d",ADC_Values[0][1]);
